62bcd6c5f6b729f461d27383a5f5c4f306740792
Adarsh Sthapit <duplicatemomo@gmail.com>
2025-11-18 21:21:27 +0545

    Add sizes route

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,9 @@
 <?php
 
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\SizeController;
 use App\Http\Controllers\Admin\AdminController;
 use App\Http\Controllers\Admin\ColorController;
-use Illuminate\Support\Facades\Route;
 
 Route::get('/', [AdminController::class, 'login'])->name('admin.login');
 Route::post('admin/auth', [AdminController::class, 'auth'])->name('admin.auth');
@@ -12,7 +13,7 @@ Route::middleware('admin')->group(function () {
         Route::get('dashboard', [AdminController::class, 'index'])
             ->name('admin.index');
         Route::post('logout', [AdminController::class, 'logout'])->name('admin.logout');
-        
+
         //colors routes
         Route::resource('colors', ColorController::class, [
             'names' => [
@@ -22,6 +23,18 @@ Route::middleware('admin')->group(function () {
                 'edit' => 'admin.colors.edit',
                 'update' => 'admin.colors.update',
                 'destroy' => 'admin.colors.destroy',
+            ]
+        ]);
+
+        //sizes routes
+        Route::resource('sizes', SizeController::class, [
+            'names' => [
+                'index' => 'admin.sizes.index',
+                'create' => 'admin.sizes.create',
+                'store' => 'admin.sizes.store',
+                'edit' => 'admin.sizes.edit',
+                'update' => 'admin.sizes.update',
+                'destroy' => 'admin.sizes.destroy',
             ]
         ]);
     });
