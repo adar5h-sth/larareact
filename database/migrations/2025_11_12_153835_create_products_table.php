985ef505d886bc2697067b747112ae7dba29b18a
Adarsh Sthapit <duplicatemomo@gmail.com>
2025-11-20 21:57:31 +0545

    Changed datatype for status to boolean

--- database/migrations/2025_11_12_153835_create_products_table.php
+++ database/migrations/2025_11_12_153835_create_products_table.php
@@ -22,7 +22,7 @@
                 $table->string('first_image')->nullable();
                 $table->string('second_image')->nullable();
                 $table->string('third_image')->nullable();
-                $table->string('status')->default(1);
+                $table->boolean('status')->default(1);
                 $table->timestamps();
             });
         }
